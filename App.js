82d03ade23ae830d2beb9d8bd711451a11cba08e
Eke Chinedu <ekechinedu488@gmail.com>
2020-09-29 14:38:35 +0100

    added components

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Index from './src/Index';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Index />
       <StatusBar style="auto" />
     </View>
   );
