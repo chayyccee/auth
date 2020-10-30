0ad1a03eb099aa4034fc1f0dac743bf75e06dded
Eke Chinedu <ekechinedu488@gmail.com>
2020-10-30 17:48:28 +0100

    upgraded expo package and installed firebase

--- src/Index.js
+++ src/Index.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class Index extends Component {
+    componentDidMount() {
+        
+    }
     render() {
         return(
             <View>
